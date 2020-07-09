be5aa9a8d9cbd934ea29ef2616b9593aefd3cf6a
minWachya <61674991+minWachya@users.noreply.github.com>
2020-07-09 14:22:15 +0900

    Visitor_Pattern

--- Entry.java
+++ Entry.java
@@ -1,17 +1,18 @@
-package test;
+package ch13;
+import java.util.Iterator;
 
-public abstract class Entry {
-	public abstract String getName();	//이름을 얻는다.
-	public abstract int getSize();		//크기를 얻는다.
-	public Entry add(Entry entry) throws FileTreatmentException {
-		//엔트리를 추가한다.
+public abstract class Entry implements Element {
+	public abstract String getName();								//이름을 얻는다.
+	public abstract int getSize();									//크기를 얻는다.
+	
+	public Entry add(Entry entry) throws FileTreatmentException {	//엔트리를 추가한다.
 		throw new FileTreatmentException();
 	}
-	public void printList() {	//일람을 표시한다.
-		printList("");
+	
+	public Iterator iterator() throws FileTreatmentException {		//Iterator 생성
+		throw new FileTreatmentException();
 	}
-	protected abstract void printList(String prefix);	//prefix를 앞에 붙여서 종류를 표시한다.
-	public String toString() {	//문자열 표현
+	public String toString() {										//문자열 표현
 		return getName() + " <" + getSize() + ">";
 	}
 }
--- FileTreatmentException.java
+++ FileTreatmentException.java
@@ -1,9 +1,8 @@
-package test;
+package ch13;
 
 public class FileTreatmentException extends RuntimeException {
 	public FileTreatmentException() {}
 	public FileTreatmentException(String msg) {
 		super(msg);
 	}
-	
 }
