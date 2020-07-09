4e9b65e8902acc2bfbcf7159a6bdd3f4a941a61e
minWachya <61674991+minWachya@users.noreply.github.com>
2020-07-09 15:37:12 +0900

    ChainOfResponsibility_Pattern

---
+++ Ch14_Chain_of_Responsibility_Pattern/Trouble.java
@@ -0,0 +1,16 @@
+package ch14;
+
+public class Trouble {
+	private int number;				//트러블 번호
+	
+	public Trouble(int number) {	//트러블 생성
+		this.number = number;
+	}
+	
+	public int getNumber() {		//트러블 넘버를 얻는다.
+		return number;
+	}
+	public String toString() {		//트러블의 문자열 표현
+		return "[Trouble " + number + "]";
+	}
+}
