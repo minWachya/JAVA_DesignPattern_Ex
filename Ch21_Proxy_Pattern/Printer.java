fa98bcdd563f7fdd8c1e48c9527684fa583704a5
minWachya <61674991+minWachya@users.noreply.github.com>
2020-07-10 20:27:20 +0900

    Proxy_Pattern

---
+++ Ch21_Proxy_Pattern/Printer.java
@@ -0,0 +1,38 @@
+package ch21;
+
+public class Printer implements Printable {
+	private String name;
+	
+	public Printer() {
+		heavyJob("Printer의 인스턴스를 생성 중");
+	}
+	public Printer(String name) {
+		this.name = name;
+		heavyJob("Priner의 인스턴스 (" + name + ")을 생성 중");
+	}
+	
+	@Override
+	public void setPrinterName(String name) {		//이름의 설정
+		this.name = name;
+	}
+	@Override
+	public String getPrinterName() {				//이름의 취득
+		return name;
+	}
+	@Override
+	public void print(String string) {				//이름을 붙여서 표시
+		System.out.println("===" + name + "===");
+		System.out.println(string);
+	}
+	private void heavyJob(String msg) {				//무거운 일(의 예정)
+		System.out.print(msg);
+		for(int i = 0; i < 5; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+			System.out.print(".");
+		}
+		System.out.println("완료.");
+	}
+}
