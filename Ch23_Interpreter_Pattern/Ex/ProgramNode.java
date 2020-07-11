5e309677c08bfce3cd52b24a135ba4766b349c65
minWachya <61674991+minWachya@users.noreply.github.com>
2020-07-11 18:17:57 +0900

    Ex_Interpreter_Pattern

---
+++ Ch23_Interpreter_Pattern/Ex/ProgramNode.java
@@ -0,0 +1,19 @@
+package language;
+
+//<program> ::= program <command list>
+public class ProgramNode extends Node {
+	private Node commandListNode;
+	
+	@Override
+	public void parse(Context context) throws ParseException {
+		context.skipToken("program");
+		commandListNode = new CommandListNode();
+		commandListNode.parse(context);
+	}
+	public void execute() throws ExecuteException {
+		commandListNode.execute();
+	}
+	public String toString() {
+		return "[program " + commandListNode + "]";
+	}
+}
