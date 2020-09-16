b1605bf5a11a78ad5cf72e174ba9ae92abaaf38a
minWachya <61674991+minWachya@users.noreply.github.com>
2020-09-16 15:59:06 +0900

    Create Main.java

---
+++ MY_DesignPattern/ch02.Adapter1/Main.java
@@ -0,0 +1,13 @@
+package AdapterPattern1;
+
+// Adapter 클래스
+public class Adapter extends Charger_microUSB implements Charger_TypeC {
+	public Adapter(String device) {
+		super(device);
+	}
+	
+	@Override						// 5핀 충전기 --> 어댑터 --> C타입 충전기 --> C타입 충전기를 가진 기기 충전하기
+	public void charging_TypeC() {	// C타입 충전기로 충전하는 메소드
+		charging_microUSB();		// 진짜 충전은 5핀 충전기가 함
+	}
+}
