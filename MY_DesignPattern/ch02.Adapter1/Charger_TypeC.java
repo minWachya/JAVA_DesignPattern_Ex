b938349f49da34d10b01261560da9759f8f720a4
minWachya <61674991+minWachya@users.noreply.github.com>
2020-09-16 15:59:26 +0900

    Add files via upload

---
+++ MY_DesignPattern/ch02.Adapter1/Charger_TypeC.java
@@ -0,0 +1,6 @@
+package AdapterPattern1;
+
+//내(클라인언트, Main)가 필요로 하는  메소드가 선언되어진 인터페이스
+public interface Charger_TypeC {
+	public abstract void charging_TypeC();	// C타입 충전기로 충전하는 메소드
+}
---
+++ MY_DesignPattern/ch02.Adapter1/Charger_microUSB.java
@@ -0,0 +1,13 @@
+package AdapterPattern1;
+// 5핀 충전기(내가 지금 갖고 있는 것)
+public class Charger_microUSB {
+	private String device = "";		// 장치 이름
+	
+	public Charger_microUSB(String device) {	// 장치 이름을 인자로 받으면서 성생
+		this.device = device;
+	}
+	
+	public void charging_microUSB() {	// 5핀 충전기로 충전하는 메소드
+		System.out.println("---" + device + " 충전중 ---");
+	}
+}
